541f8481b620798af73fcfdf258ae00d81a59d2f
satwikj218 <satwikj21@gmail.com>
2024-04-02 16:05:50 -0400

    creating application

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, { message: "Name is too short"}),
+    username: z.string().min(2, { message: "Username is too short"}),
+    email: z.string().email({ message: "Please enter a valid email"}),
+    password: z.string().min(8, { message: "Password must be atleast 8 characters"}),
+  })
+  
